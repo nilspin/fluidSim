50d988b2166739cd3a8d51ea4d7b016c78cca168
Nilay Engineer <gooney28@gmail.com>
2015-07-03 19:21:31 +0530

    Added OpenGL version number.

--- SDL_OpenGL_Project/SDL_OpenGL_Project.cpp
+++ SDL_OpenGL_Project/SDL_OpenGL_Project.cpp
@@ -28,6 +28,8 @@ int main(int argc, char *argv[])
 
 	SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 16);
 	SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
+	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 4);
+	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 2);
 
 	//Init GLEW
 	GLenum err = glewInit();
